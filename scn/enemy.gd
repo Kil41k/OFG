d36f74a9438a6f82d933152dbd5a9c6b66729907
Diyas Tuglukov <tuglukov.7.diyas@gmail.com>
2024-01-24 22:10:29 +0600

    money

--- scn/enemy.gd
+++ scn/enemy.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
-
 @onready var hpLabel = $HP
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	hpLabel.text = str(Global.enemyHealth)
 	if Global.enemyHealth <= 0:
-		queue_free()
+		death()
 
 func _on_hit_button_up():
 	Global.enemyHealth -= Global.damage
+
+func death():
+	Global.money += Global.enemyHealth
+	queue_free()
